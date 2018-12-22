bd408ef031f601db9f378bae036ad5e371edb9ab
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-12-22 19:34:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,6 +10,7 @@ class Anagram
   
   
   def match(array)
+    binding.pry
     array.map do |string|
       if string.chars.sort == @anagram.chars.sort 
         string
@@ -17,7 +18,7 @@ class Anagram
         array.clear
       end
     end.flatten
-    binding.pry
+    
   end 
   
   
