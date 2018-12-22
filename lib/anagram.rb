e55dd88423bc90d9c8accd0ef1b5860f7332d0ad
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-12-22 19:25:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :anagram
+   
+  def initialize(word)
+    word = word
+    @anagram = word
+  end 
+  
+  def match(array)
+    array.find do |string|
+      if string.chars.sort == @anagram.chars.sort 
+        string.split
+      else 
+        array.clear
+      end
+    end 
+  end 
+  
+  
+end
