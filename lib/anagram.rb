dc2978e78e90e723565c4204c75405e57669af07
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-12-22 20:15:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :anagram
+   
+  def initialize(word)
+    word = word
+    @anagram = word
+  end 
+  
+  def match(array)
+    array.map do |string|
+      string if string.chars.sort == @anagram.chars.sort 
+    end.flatten.compact
+  end 
+  
+end
